1e382003725cce9c3ea220363a4401994c311611
Matthew <MattJTrueblood@gmail.com>
2016-03-05 15:23:43 -0700

    Added tag adding/index list creation

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@
  * Created by Matthew on 3/5/2016.
  */
 
+//items is the master list of all items.  The only things that can change this are adding, removing, and loading.
+var masterItems = [];
+//tags is the master list of all tags.  The only things that can change this are adding, removing, and loading.
+var masterTags = [];
+
+
+
 
 /**
  * Item is a constructor for an Item object.  It's the basic organizational element.
@@ -17,11 +24,66 @@ function Item(name, description, tags, startTime, endTime) {
     var that = this;
     that.name = name;
     that.description = description;
-    that.tags = tags.split(" ");
+    that.tags = getTagIndices(tags);
     that.startTime = startTime;
     that.endTime = endTime;
 
-    //methods
-
     return that;
 }
+
+/**
+ * Takes in the input-formatted (string) "tags".  Returns a list of indices into the master tags array,
+ * adding new tags to the master tags array if they don't already exist.
+ * @param tagsIn
+ * @returns {Array}
+ */
+function getTagIndices(tagsIn) {
+    //Get the array of tags instead of an input string.
+    var tagsList = tagsIn.split(" ");
+    var indices = [];
+    //If the tag already exists, push its index.  Otherwise, add it as a new tag, then push its index.
+    for(var i = 0; i < tagsList.length; i++) {
+        //Edge case when there are no tags right now
+        if(masterTags.length == 0) {
+            addNewTag(tagsList[i]);
+            indices.push(0);
+        }
+        else { //Normal case.
+            for (var j = 0; j < masterTags.length; j++) {
+                if (tagsList[i] == masterTags[j]) {
+                    //The tag is there, give the index.
+                    indices.push(j);
+                    break; //no need to stay in the loop, you've already found the index.  Next tag!
+                }
+                if (j == masterTags.length - 1) {
+                    //if this is the last tag in the master list, it's not here.  Add the tag!
+                    addNewTag(tagsList[i]);
+                    //Now, the function will loop through one more time, since tags is one element bigger.
+                    //It will hit the first condition, and push the correct index to the indices array.
+                }
+            }
+        }
+    }
+    return indices;
+}
+
+/**
+ * Adds a new tag in the master tag array.
+ * @param tagName
+ */
+function addNewTag(tagName) {
+    masterTags.push(tagName);
+}
+
+console.log("adding tag1 and tag2 to indices:   " + getTagIndices("tag1 tag2").map(String));
+console.log("current tag list:  " + masterTags);
+console.log("adding tag2 and tag3 to indices:   " + getTagIndices("tag2 tag3").map(String));
+console.log("current tag list:  " + masterTags);
+console.log("adding special tag2, tag3, tag1 to indices:   " + getTagIndices("special tag2 tag3 tag1").map(String));
+console.log("current tag list:  " + masterTags);
+
+
+
+
+
+
