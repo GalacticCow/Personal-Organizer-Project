0fb302cb69449f9a40d426e2e1a7b40341f26298
Matthew <MattJTrueblood@gmail.com>
2016-03-05 14:38:02 -0700

    added base Item constructor

--- scripts.js
+++ scripts.js
@@ -1,3 +1,27 @@
 /**
  * Created by Matthew on 3/5/2016.
  */
+
+
+/**
+ * Item is a constructor for an Item object.  It's the basic organizational element.
+ * Item(...) returns an item that does great awesome fun stuff.
+ * @param name
+ * @param description
+ * @param tags
+ * @param startTime
+ * @param endTime
+ * @constructor
+ */
+function Item(name, description, tags, startTime, endTime) {
+    var that = this;
+    that.name = name;
+    that.description = description;
+    that.tags = tags.split(" ");
+    that.startTime = startTime;
+    that.endTime = endTime;
+
+    //methods
+
+    return that;
+}
